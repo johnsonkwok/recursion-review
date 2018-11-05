f79b8c1cb47ddb06d98101c0914650da4aa185a1
johnsonkwok <johnsonkwok1@gmail.com>
2018-11-05 13:43:08 -0800

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null || Number.isNaN(obj) || obj === Infinity || obj === -Infinity) {
+    return "null";
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return String(obj);
+  } else if (typeof obj === 'string') {
+    return `"${obj}"`;
+  } else if (Array.isArray(obj)) {
+    let results = [];
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i] === undefined || typeof obj === 'function') {
+        results.push('null');
+      } else {
+        results.push(stringifyJSON(obj[i]));
+      }    
+    }
+    return '[' + results.join(',') + ']';
+  } else if (typeof obj === 'object') {
+    let results = [];
+    for (let key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + results.join(',') + '}';
+  } else {
+    return 'Invalid JSON';  
+  }
 };
